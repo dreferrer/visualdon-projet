8a69be3910d9b6c6a588b15b0b9d08f14a448121
Andrea Ferrer <andrea.ferrer@heig-vd.ch>
2023-04-21 08:32:38 +0200

    Adding datasets

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log('Hello World 2!')
+import { select } from 'd3-selection';
+import { csv} from 'd3-fetch';
+
+const dataCrash = csv('../data/crashDataSet.csv');
+const dataLastWords = csv('../data/lastWords.csv');
+
+console.log(dataCrash);  
+console.log(dataLastWords);
