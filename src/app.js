1253138b9c916b61f6d2338e1be0f0ff6c9996d8
Andrea Ferrer <andrea.ferrer@heig-vd.ch>
2023-04-21 12:09:37 +0200

    Adding new elements

--- src/app.js
+++ src/app.js
@@ -1,8 +1,2 @@
 import { select } from 'd3-selection';
-import { csv} from 'd3-fetch';
-
-const dataCrash = csv('../data/crashDataSet.csv');
-const dataLastWords = csv('../data/lastWords.csv');
-
-console.log(dataCrash);  
-console.log(dataLastWords);
+import { csv } from 'd3-fetch';
