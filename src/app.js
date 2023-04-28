f15a21aeabe0aa8b163fe8b2b0a1ea82bc59f7da
Andrea Ferrer <andrea.ferrer@heig-vd.ch>
2023-04-28 14:11:23 +0200

    Import data

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
 import { select } from 'd3-selection';
 import { csv } from 'd3-fetch';
+
+const crashData = csv('./data/crashDataSet.csv');
+const lastWordsData = csv('./data/lastWords.csv');
+
+
+
